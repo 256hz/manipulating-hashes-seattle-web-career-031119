b0f7bd85d332e2503fc50eb5b8996f2aa4812e02
Abe D <iaryiou111@gmail.com>
2019-03-05 17:01:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,8 @@ def first_challenge
       contacts["Freddy Mercury"][k].each do |flavor|
         if flavor = "strawberry"
           delete contacts["Freddy Mercury"][k][flavor]
+        end
+      end
     end
   end
   #remember to return your newly altered contacts hash!
