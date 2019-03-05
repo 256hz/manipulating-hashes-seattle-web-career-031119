90036acc471c71f65aaef89795bcd196df2932f0
Abe D <iaryiou111@gmail.com>
2019-03-05 17:06:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,9 +15,7 @@ def first_challenge
   
   contacts["Freddy Mercury"].each do |k, v|
     if k = :favorite_icecream_flavors
-      contacts["Freddy Mercury"][k].each.with_index do |flavor, index|
-        contacts["Freddy Mercury"][k][index].delete_if{flavor = "strawberry"}
-      end
+      contacts["Freddy Mercury"][k].delete_if{flavor = "strawberry"}
     end
   end
   #remember to return your newly altered contacts hash!
